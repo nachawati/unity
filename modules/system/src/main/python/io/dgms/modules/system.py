ef568bf19eec0671a3f5cb8af1ff2e76a41984fe
Mohamad Omar Nachawati <mnachawa@gmu.edu>
2019-05-08 02:44:39 -0400

    Update system.py

--- modules/system/src/main/python/io/dgms/modules/system.py
+++ modules/system/src/main/python/io/dgms/modules/system.py
@@ -27,5 +27,437 @@
 
 import os
 
+def abort(*args, **kwargs):
+    return os.abort(*args, **kwargs)
+
+def access(*args, **kwargs):
+    return os.access(*args, **kwargs)
+
+def chdir(*args, **kwargs):
+    return os.chdir(*args, **kwargs)
+
+def chflags(*args, **kwargs):
+    return os.chflags(*args, **kwargs)
+
+def chmod(*args, **kwargs):
+    return os.chmod(*args, **kwargs)
+
 def chown(*args, **kwargs):
     return os.chown(*args, **kwargs)
+
+def chroot(*args, **kwargs):
+    return os.chroot(*args, **kwargs)
+
+def close(*args, **kwargs):
+    return os.close(*args, **kwargs)
+
+def cpu_count(*args, **kwargs):
+    return os.cpu_count(*args, **kwargs)
+
+def ctermid(*args, **kwargs):
+    return os.ctermid(*args, **kwargs)
+
+def dup(*args, **kwargs):
+    return os.dup(*args, **kwargs)
+
+def dup2(*args, **kwargs):
+    return os.dup2(*args, **kwargs)
+
+def execl(*args, **kwargs):
+    return os.execl(*args, **kwargs)
+
+def execle(*args, **kwargs):
+    return os.execle(*args, **kwargs)
+
+def execlp(*args, **kwargs):
+    return os.execlp(*args, **kwargs)
+
+def execlpe(*args, **kwargs):
+    return os.execlpe(*args, **kwargs)
+
+def execv(*args, **kwargs):
+    return os.execv(*args, **kwargs)
+
+def execve(*args, **kwargs):
+    return os.execve(*args, **kwargs)
+
+def execvp(*args, **kwargs):
+    return os.execvp(*args, **kwargs)
+
+def execvpe(*args, **kwargs):
+    return os.execvpe(*args, **kwargs)
+
+def exit(*args, **kwargs):
+    return os._exit(*args, **kwargs)
+
+def fchdir(*args, **kwargs):
+    return os.fchdir(*args, **kwargs)
+
+def fchmod(*args, **kwargs):
+    return os.fchmod(*args, **kwargs)
+
+def fchown(*args, **kwargs):
+    return os.fchown(*args, **kwargs)
+
+def fdatasync(*args, **kwargs):
+    return os.fdatasync(*args, **kwargs)
+
+def fdopen(*args, **kwargs):
+    return os.fdopen(*args, **kwargs)
+
+def fork(*args, **kwargs):
+    return os.fork(*args, **kwargs)
+
+def forkpty(*args, **kwargs):
+    return os.forkpty(*args, **kwargs)
+
+def fpathconf(*args, **kwargs):
+    return os.fpathconf(*args, **kwargs)
+
+def fsdecode(*args, **kwargs):
+    return os.fsdecode(*args, **kwargs)
+
+def fsencode(*args, **kwargs):
+    return os.fsencode(*args, **kwargs)
+
+def fspath(*args, **kwargs):
+    return os.fspath(*args, **kwargs)
+
+def fstat(*args, **kwargs):
+    return os.fstat(*args, **kwargs)
+
+def fstatvfs(*args, **kwargs):
+    return os.fstatvfs(*args, **kwargs)
+
+def fsync(*args, **kwargs):
+    return os.fsync(*args, **kwargs)
+
+def ftruncate(*args, **kwargs):
+    return os.ftruncate(*args, **kwargs)
+
+def fwalk(*args, **kwargs):
+    return os.fwalk(*args, **kwargs)
+
+def getcwd(*args, **kwargs):
+    return os.getcwd(*args, **kwargs)
+
+def getcwdb(*args, **kwargs):
+    return os.getcwdb(*args, **kwargs)
+
+def getegid(*args, **kwargs):
+    return os.getegid(*args, **kwargs)
+
+def getenv(*args, **kwargs):
+    return os.getenv(*args, **kwargs)
+
+def getenvb(*args, **kwargs):
+    return os.getenvb(*args, **kwargs)
+
+def geteuid(*args, **kwargs):
+    return os.geteuid(*args, **kwargs)
+
+def getgid(*args, **kwargs):
+    return os.getgid(*args, **kwargs)
+
+def getgrouplist(*args, **kwargs):
+    return os.getgrouplist(*args, **kwargs)
+
+def getgroups(*args, **kwargs):
+    return os.getgroups(*args, **kwargs)
+
+def getloadavg(*args, **kwargs):
+    return os.getloadavg(*args, **kwargs)
+
+def getlogin(*args, **kwargs):
+    return os.getlogin(*args, **kwargs)
+
+def getpgid(*args, **kwargs):
+    return os.getpgid(*args, **kwargs)
+
+def getpgrp(*args, **kwargs):
+    return os.getpgrp(*args, **kwargs)
+
+def getpid(*args, **kwargs):
+    return os.getpid(*args, **kwargs)
+
+def getppid(*args, **kwargs):
+    return os.getppid(*args, **kwargs)
+
+def getpriority(*args, **kwargs):
+    return os.getpriority(*args, **kwargs)
+
+def getrandom(*args, **kwargs):
+    return os.getrandom(*args, **kwargs)
+
+def getresgid(*args, **kwargs):
+    return os.getresgid(*args, **kwargs)
+
+def getresuid(*args, **kwargs):
+    return os.getresuid(*args, **kwargs)
+
+def getsid(*args, **kwargs):
+    return os.getsid(*args, **kwargs)
+
+def getuid(*args, **kwargs):
+    return os.getuid(*args, **kwargs)
+
+def initgroups(*args, **kwargs):
+    return os.initgroups(*args, **kwargs)
+
+def isatty(*args, **kwargs):
+    return os.isatty(*args, **kwargs)
+
+def kill(*args, **kwargs):
+    return os.kill(*args, **kwargs)
+
+def killpg(*args, **kwargs):
+    return os.killpg(*args, **kwargs)
+
+def lchflags(*args, **kwargs):
+    return os.lchflags(*args, **kwargs)
+
+def lchmod(*args, **kwargs):
+    return os.lchmod(*args, **kwargs)
+
+def lchown(*args, **kwargs):
+    return os.lchown(*args, **kwargs)
+
+def link(*args, **kwargs):
+    return os.link(*args, **kwargs)
+
+def listdir(*args, **kwargs):
+    return os.listdir(*args, **kwargs)
+
+def lockf(*args, **kwargs):
+    return os.lockf(*args, **kwargs)
+
+def lseek(*args, **kwargs):
+    return os.lseek(*args, **kwargs)
+
+def lstat(*args, **kwargs):
+    return os.lstat(*args, **kwargs)
+
+def major(*args, **kwargs):
+    return os.major(*args, **kwargs)
+
+def makedev(*args, **kwargs):
+    return os.makedev(*args, **kwargs)
+
+def makedirs(*args, **kwargs):
+    return os.makedirs(*args, **kwargs)
+
+def minor(*args, **kwargs):
+    return os.minor(*args, **kwargs)
+
+def mkdir(*args, **kwargs):
+    return os.mkdir(*args, **kwargs)
+
+def mkfifo(*args, **kwargs):
+    return os.mkfifo(*args, **kwargs)
+
+def mknod(*args, **kwargs):
+    return os.mknod(*args, **kwargs)
+
+def name(*args, **kwargs):
+    return os.name
+
+def nice(*args, **kwargs):
+    return os.nice(*args, **kwargs)
+
+def open(*args, **kwargs):
+    return os.open(*args, **kwargs)
+
+def openpty(*args, **kwargs):
+    return os.openpty(*args, **kwargs)
+
+def pathconf(*args, **kwargs):
+    return os.pathconf(*args, **kwargs)
+
+def pipe(*args, **kwargs):
+    return os.pipe(*args, **kwargs)
+
+def pipe2(*args, **kwargs):
+    return os.pipe2(*args, **kwargs)
+
+def plock(*args, **kwargs):
+    return os.plock(*args, **kwargs)
+
+def popen(*args, **kwargs):
+    return os.popen(*args, **kwargs)
+
+def pread(*args, **kwargs):
+    return os.pread(*args, **kwargs)
+
+def preadv(*args, **kwargs):
+    return os.preadv(*args, **kwargs)
+
+def putenv(*args, **kwargs):
+    return os.putenv(*args, **kwargs)
+
+def pwrite(*args, **kwargs):
+    return os.pwrite(*args, **kwargs)
+
+def pwritev(*args, **kwargs):
+    return os.pwritev(*args, **kwargs)
+
+def read(*args, **kwargs):
+    return os.read(*args, **kwargs)
+
+def readlink(*args, **kwargs):
+    return os.readlink(*args, **kwargs)
+
+def readv(*args, **kwargs):
+    return os.readv(*args, **kwargs)
+
+def remove(*args, **kwargs):
+    return os.remove(*args, **kwargs)
+
+def removedirs(*args, **kwargs):
+    return os.removedirs(*args, **kwargs)
+
+def rename(*args, **kwargs):
+    return os.rename(*args, **kwargs)
+
+def renames(*args, **kwargs):
+    return os.renames(*args, **kwargs)
+
+def replace(*args, **kwargs):
+    return os.replace(*args, **kwargs)
+
+def rmdir(*args, **kwargs):
+    return os.rmdir(*args, **kwargs)
+
+def scandir(*args, **kwargs):
+    return os.scandir(*args, **kwargs)
+
+def setegid(*args, **kwargs):
+    return os.setegid(*args, **kwargs)
+
+def seteuid(*args, **kwargs):
+    return os.seteuid(*args, **kwargs)
+
+def setgid(*args, **kwargs):
+    return os.setgid(*args, **kwargs)
+
+def setgroups(*args, **kwargs):
+    return os.setgroups(*args, **kwargs)
+
+def setpgid(*args, **kwargs):
+    return os.setpgid(*args, **kwargs)
+
+def setpgrp(*args, **kwargs):
+    return os.setpgrp(*args, **kwargs)
+
+def setpriority(*args, **kwargs):
+    return os.setpriority(*args, **kwargs)
+
+def setregid(*args, **kwargs):
+    return os.setregid(*args, **kwargs)
+
+def setresgid(*args, **kwargs):
+    return os.setresgid(*args, **kwargs)
+
+def setresuid(*args, **kwargs):
+    return os.setresuid(*args, **kwargs)
+
+def setreuid(*args, **kwargs):
+    return os.setreuid(*args, **kwargs)
+
+def setsid(*args, **kwargs):
+    return os.setsid(*args, **kwargs)
+
+def setuid(*args, **kwargs):
+    return os.setuid(*args, **kwargs)
+
+def spawnl(*args, **kwargs):
+    return os.spawnl(*args, **kwargs)
+
+def spawnle(*args, **kwargs):
+    return os.spawnle(*args, **kwargs)
+
+def spawnlp(*args, **kwargs):
+    return os.spawnlp(*args, **kwargs)
+
+def spawnlpe(*args, **kwargs):
+    return os.spawnlpe(*args, **kwargs)
+
+def spawnv(*args, **kwargs):
+    return os.spawnv(*args, **kwargs)
+
+def spawnve(*args, **kwargs):
+    return os.spawnve(*args, **kwargs)
+
+def spawnvp(*args, **kwargs):
+    return os.spawnvp(*args, **kwargs)
+
+def spawnvpe(*args, **kwargs):
+    return os.spawnvpe(*args, **kwargs)
+
+def startfile(*args, **kwargs):
+    return os.startfile(*args, **kwargs)
+
+def stat(*args, **kwargs):
+    return os.stat(*args, **kwargs)
+
+def statvfs(*args, **kwargs):
+    return os.statvfs(*args, **kwargs)
+
+def symlink(*args, **kwargs):
+    return os.symlink(*args, **kwargs)
+
+def sync(*args, **kwargs):
+    return os.sync(*args, **kwargs)
+
+def system(*args, **kwargs):
+    return os.system(*args, **kwargs)
+
+def times(*args, **kwargs):
+    return os.times(*args, **kwargs)
+
+def truncate(*args, **kwargs):
+    return os.truncate(*args, **kwargs)
+
+def ttyname(*args, **kwargs):
+    return os.ttyname(*args, **kwargs)
+
+def umask(*args, **kwargs):
+    return os.umask(*args, **kwargs)
+
+def uname(*args, **kwargs):
+    return os.uname(*args, **kwargs)
+
+def unlink(*args, **kwargs):
+    return os.unlink(*args, **kwargs)
+
+def unsetenv(*args, **kwargs):
+    return os.unsetenv(*args, **kwargs)
+
+def urandom(*args, **kwargs):
+    return os.urandom(*args, **kwargs)
+
+def utime(*args, **kwargs):
+    return os.utime(*args, **kwargs)
+
+def wait(*args, **kwargs):
+    return os.wait(*args, **kwargs)
+
+def wait3(*args, **kwargs):
+    return os.wait3(*args, **kwargs)
+
+def wait4(*args, **kwargs):
+    return os.wait4(*args, **kwargs)
+
+def waitid(*args, **kwargs):
+    return os.waitid(*args, **kwargs)
+
+def waitpid(*args, **kwargs):
+    return os.waitpid(*args, **kwargs)
+
+def walk(*args, **kwargs):
+    return os.walk(*args, **kwargs)
+
+def write(*args, **kwargs):
+    return os.write(*args, **kwargs)
+
+def writev(*args, **kwargs):
+    return os.writev(*args, **kwargs)
