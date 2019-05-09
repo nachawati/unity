851dc16582b039b442086ad28bd2b6b1072c7c64
Mohamad Omar Nachawati <mnachawa@gmu.edu>
2019-05-09 05:12:42 -0400

    Add Lab.java

---
+++ cli/src/main/java/unity/cli/Lab.java
@@ -0,0 +1,50 @@
+/*
+ * بسم الله الرحمن الرحيم 
+ *
+ * In the name of Allah, the Most Compassionate, the Most Merciful
+ *
+ * This file is part of Unity DGMS <https://www.dgms.io/>
+ *
+ * Unity DGMS is free software; redistribution and use in source and binary
+ * forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above notice, this list of
+ *    conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above notice, this list
+ *    of conditions and the following disclaimer in the documentation and/or
+ *    other materials provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHORS DISCLAIM ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package unity.cli;
+
+import picocli.CommandLine.Command;
+import unity.kernel.engine.UnityAnalyticsEngineManager;
+
+@Command(name = "lab", description = "Start lab session")
+public class Lab extends Main.Command
+{
+    @Override
+    public void exec() throws Exception
+    {
+        final ProcessBuilder processBuilder = new ProcessBuilder("jupyter", "lab", "--allow-root", "--i=0.0.0.0");
+        processBuilder.environment().put("JUPYTER_PATH", UnityAnalyticsEngineManager.getInstallPath().toString());
+        processBuilder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
+        processBuilder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        final Process p = processBuilder.start();
+        try {
+            p.waitFor();
+        } catch (final InterruptedException e) {
+        }
+    }
+}
