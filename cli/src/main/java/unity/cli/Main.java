5c7049d58ed6a85d05de30374d7238600948a98c
Mohamad Omar Nachawati <mnachawa@gmu.edu>
2019-05-13 03:48:54 -0400

    Update Main.java

--- cli/src/main/java/unity/cli/Main.java
+++ cli/src/main/java/unity/cli/Main.java
@@ -60,7 +60,7 @@ import unity.kernel.engine.UnityAnalyticsContext;
         mixinStandardHelpOptions = true,
         abbreviateSynopsis = true,
         versionProvider = Main.class,
-        subcommands = { Compile.class, Console.class, Generate.class, Kernel.class, Lab.class, Notebook.class, Run.class, Server.class })
+        subcommands = { Compile.class, Console.class, Generate.class, Kernel.class, Lab.class, Login.class, Logout.class, Notebook.class, Run.class, Server.class, Whoami.class })
 public class Main implements IVersionProvider, Runnable
 {
     public static synchronized String getVersionString()
